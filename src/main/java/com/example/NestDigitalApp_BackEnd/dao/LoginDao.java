71034efdca6c33dac4913d1dc548070c0d613aff
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-13 00:53:00 +0530

    viewlog

--- src/main/java/com/example/NestDigitalApp_BackEnd/dao/LoginDao.java
+++ src/main/java/com/example/NestDigitalApp_BackEnd/dao/LoginDao.java
@@ -2,9 +2,15 @@ package com.example.NestDigitalApp_BackEnd.dao;
 
 import com.example.NestDigitalApp_BackEnd.model.EmpLog;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LoginDao extends CrudRepository<EmpLog,Integer> {
+    @Query(value = "SELECT `id`, `date`, `empid`, `entry_datetime`, `exit_datetime` FROM `emplogin` WHERE `date`=:date",nativeQuery = true)
+    List<EmpLog> ViewDailyEmployeeLog(@Param("date") String date);
 
 
 }
