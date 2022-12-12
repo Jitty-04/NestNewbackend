c4516523a9d0b0916bcfa970be1ed414829cc181
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-12 23:30:00 +0530

    viewentry

--- src/main/java/com/example/NestDigitalApp_BackEnd/controller/LeaveController.java
+++ src/main/java/com/example/NestDigitalApp_BackEnd/controller/LeaveController.java
@@ -1,7 +1,40 @@
 package com.example.NestDigitalApp_BackEnd.controller;
 
+import com.example.NestDigitalApp_BackEnd.dao.LiveApplicationDao;
+import com.example.NestDigitalApp_BackEnd.dao.VisitorDao;
+import com.example.NestDigitalApp_BackEnd.model.EmpLog;
+import com.example.NestDigitalApp_BackEnd.model.LeaveApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class LeaveController {
+    @Autowired
+    private LiveApplicationDao dao;
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/leaveapp",consumes = "application/json",produces = "application/json")
+    public Map<String,String> Leaveapp(@RequestBody LeaveApplication l)
+    {
+        System.out.println(l.getId());
+        System.out.println(l.getEmpid());
+        System.out.println(l.getLeavetype().toString());
+        System.out.println(l.getApply_date().toString());
+        System.out.println(l.getFrom_date().toString());
+        System.out.println(l.getTo_date().toString());
+        System.out.println(l.getRemarks().toString());
+        System.out.println(l.getStatus().toString());
+        dao.save(l);
+        HashMap<String,String> map=new HashMap<>();
+        map.put("status","success");
+        return map;
+    }
+
+
 }
